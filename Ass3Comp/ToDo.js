d9a99be516f64354a0fd3946626e4d37ec6791f7
ibrahim957 <59818665+ibrahim957@users.noreply.github.com>
2021-08-30 17:18:32 +0500

    Add files via upload

---
+++ Ass3Comp/ToDo.js
@@ -0,0 +1,100 @@
+
+let input = document.getElementById("myInput");
+let date= document.getElementById("date");
+let list = document.getElementById("list");
+
+let to_do_list =[];
+
+let setting = {weekday:"long",month:"short",day:"numeric"}
+let today = new Date();
+
+date.innerHTML=today.toLocaleDateString("en-US",setting)
+
+var listt = document.querySelector('ul');
+listt.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement()
+{
+    let txt=input.value;
+    if(txt)
+        addToDo(txt)
+	else
+		alert("You must write something!");
+    input.value="";
+}
+
+function addToDo(txt){
+	let obj={
+	data : `${txt}`,
+	strike : 0
+	};
+    to_do_list.push(obj);
+
+    let position = "beforeend"
+    let text=`  <li class="item" >
+                    <p>
+                        ${txt}
+                        <button type="button" class="link pull-right" onclick="delete_item(${to_do_list.length-1})">  <i class="fa fa-remove"></i></button>
+                        <button type="button" class="link pull-right" onclick="check_item(${to_do_list.length-1})"> <i class="fa fa-strikethrough"></i> </button>
+                    </p>
+                </li>`
+        list.insertAdjacentHTML(position,text)
+}
+
+function rebuild()
+{
+    let position = "beforeend"
+    let text = ''
+    list.innerHTML = ''
+    to_do_list.forEach(function(item,index){
+		if(item.strike == 0)
+			text= `  <li class="item" >
+                       <p>
+                            ${item.data}
+                            <button type="button" class="pull-right" onclick="delete_item(${index})">  <i class="fa fa-remove"></i></button>
+                            <button type="button" class="pull-right" onclick="check_item(${index})"> <i class="fa fa-strikethrough"></i> </button>
+                        </p>
+                    </li>`
+		else
+			text= `  <li class="item" >
+                        <p>
+                            <del>${item.data}</del>
+                            <button type="button" class="pull-right" onclick="delete_item(${index})">  <i class="fa fa-remove"></i></button>
+                            <button type="button" class="pull-right" onclick="check_item(${index})"> <i class="fa fa-strikethrough"></i> </button>
+                        </p>
+                    </li>`
+        list.insertAdjacentHTML(position,text)
+      })
+}
+
+function delete_item(index)
+{ console.log("delete_item", index)
+    to_do_list.splice(index,1);
+    rebuild();
+}
+
+function check_item(index)
+{
+    console.log("check_item", index)
+	to_do_list[index].strike= !to_do_list[index].strike;
+	rebuild();
+}
+
+
+
+document.addEventListener("keyup",function(){
+    if(event.keyCode==13){
+        let txt=input.value;
+        if(txt)
+            addToDo(txt)
+		else
+			alert("You must write something!");
+        input.value="";
+    }
+
+})
+
